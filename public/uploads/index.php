95a8488320ea1c534832c36952861d88d47db6ac
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-07 14:32:14 -0700

    Add files via upload

--- public/uploads/index.php
+++ public/uploads/index.php
@@ -2,7 +2,7 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  require('db.php');
+  require('../db.php');
   require('../functions.php');
   $asset = mysqli_real_escape_string($link, $_GET['asset']);
   if($asset){
