985382949a64268c3d309de8b0f0455a61200928
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-09 16:26:04 -0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -601,7 +601,7 @@ export default {
             let l = location.origin.toLowerCase().indexOf('000webhostapp.com') !== -1 ? 1 : 0
             if(vars[l]){
               this.state.curPage = (+vars[l])-1
-              if(''+this.state.curPage == 'NaN') this.state.curPage = 1
+              if(''+this.state.curPage == 'NaN') this.state.curPage = 0
               if(vars[l+1]){
                 this.state.search.string = decodeURIComponent(vars[l+1])
                 search = '/' + vars[l+1]
