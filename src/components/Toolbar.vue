065db6df1a098e1561df00318cb21d70573e9445
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-09 18:44:25 -0700

    Add files via upload

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -45,7 +45,7 @@
         type="text"
         ref="uploadURL"
         style="width:360px;height: calc(100% - 5px);font-size: 20px; background: #000; color: #4f8; border: 1px solid red; width: 310px"
-        @keydown.stop
+        @keydown.enter.stop="uploadByURL()"
         v-model="state.uploadFromURL"
         placeholder="upload from a URL... it might work!" class="uploadFromURL"
       >
