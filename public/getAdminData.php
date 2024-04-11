dbe1562e53d6e65942d23f2ead7919ea541ebc7d
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-11 12:47:46 -0700

    Add files via upload

--- public/getAdminData.php
+++ public/getAdminData.php
@@ -2,7 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-  require('db.php');
+  require_once('db.php');
+  require_once('functions.php');
   $data = json_decode(file_get_contents('php://input'));
   $userName = mysqli_real_escape_string($link, $data->{'userName'});
   $passhash = mysqli_real_escape_string($link, $data->{'passhash'});
