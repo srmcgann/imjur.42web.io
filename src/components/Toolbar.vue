295a54d3f8c0f097e0fc294256d52906eb89d7fd
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-14 15:51:26 -0700

    Add files via upload

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -121,9 +121,7 @@ export default {
             this.memo = [...this.memo, val]
             if(val) {
               console.log('detected code. should werk')
-              this.$nextTick(()=>{
-                el.onclick = () => {eval(val);console.log('clicky')}
-              })
+              el.onmousedown = () => {eval(val);console.log('clicky')}
             }
             el.childNodes.forEach(el2 => {this.recurseMenus(el2)})
           }
