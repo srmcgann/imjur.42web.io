7a20626ca943b86f02f95177a4b07d6801522f14
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-15 13:19:53 -0700

    Add files via upload

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -74,7 +74,7 @@
           </button>
 
         </div>
-        <div class="sub" @click="state.showCollections=true">My Collections</div>
+        <div class="sub" style="min-width: 180px;" @click="state.showCollections=true">My Collections</div>
         <div class="sub" @click="">sub b</div>
         <div class="sub" @click="">sub c</div>
         <div class="sub" @click="">
