4b1d5cb4223c1bb59d99c711c86a9520ea554d6c
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-17 12:32:24 -0700

    Add files via upload

--- src/components/CollectionSelection.vue
+++ src/components/CollectionSelection.vue
@@ -60,7 +60,7 @@ export default {
       if(!this.state.collections.length){
         this.state.showCollectionTemplate = true
       }else{
-        this.state.click = true
+        this.document.body.click()
         this.$nextTick(()=>{
           this.showCollection = !this.showCollection
         })
