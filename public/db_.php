0a7dfb8c5da1c76f859bc89c07dbcced6d724520
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-19 16:24:41 -0700

    Add files via upload

--- public/db_.php
+++ public/db_.php
@@ -1,8 +1,8 @@
 <?php 
-  $db_user  = 'if0_35882111';
-  $db_pass  = 'GNS1oMHbsX';
-  $db_host  = 'sql212.infinityfree.com';
-  $db       = "if0_36324953_imjur";
+  $db_user  = 'if0_36394634';
+  $db_pass  = '49DhSvcug8B';
+  $db_host  = 'sql309.infinityfree.com';
+  $db       = "if0_36394634_imjur";
   $port     = '3306';
   $link     = mysqli_connect($db_host,$db_user,$db_pass,$db,$port);
 ?>
