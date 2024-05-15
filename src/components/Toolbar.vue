599434e168c36486e3f9057face851b7722aa77c
Scott R McGann <s.r.mcgann@hotmail.com>
2024-05-15 16:11:06 -0700

    Add files via upload

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -149,9 +149,10 @@
     >
     <button @click="uploadByURL()" class="goButton" title="download asset by URL [enter]">go</button>
     <button
-      @click="state.mode='trending'">
+      @click="state.mode='trending'"
       v-if="state.loggedIn"
       style="margin: 5px; position: absolute; margin-top: 7px;"
+    >
       trending
     </button>
   </div>
